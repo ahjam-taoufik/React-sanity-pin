4ec54b4473b70d35403ac083b949f99687869729
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-22 22:13:54 +0100

    so far login

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Route, Router, Routes } from "react-router-dom";
+import Login from './components/Login';
+import Home from './container/Home';
+
+
+function App() {
+  return (
+     <Routes>
+       <Route path="login" element={<Login/>} />
+       <Route path="/*" element={<Home/>} />
+     </Routes>
+  );
+}
+
+export default App;
